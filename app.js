fae3192b997d76b9d79e08661de4bb9f77976133
Mcvean Soans <mc.soans02@gmail.com>
2020-09-25 19:29:45 +0530

    Express Sessions Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,12 +30,26 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+
+// Using cookie-parser and signed cookies
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+// Using Sessions instead of cookie-parser
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 // Authorization starts
 function auth (req, res, next) {
+  // Use (req.signedCookies) if cookie-parser is used
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
+  //Use (!req.signedCookies.user) for cookie-parser
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
       var err = new Error('You are not authenticated!');
@@ -49,7 +65,8 @@ function auth (req, res, next) {
     var pass = auth[1];
 
     if (user === 'admin' && pass === 'password') {
-      res.cookie('user','admin',{signed: true});
+      // Use res.cookie('user','admin',{signed: true}); if cookie-parser is used
+      req.session.user = 'admin';
       next(); // authorized to pass onto the next middleware
     }
     else {
@@ -60,7 +77,8 @@ function auth (req, res, next) {
     }
   }
   else {
-    if (req.signedCookies.user === 'admin') {
+    //Use (req.signedCookies.user) for cookie-parser
+    if (req.session.user === 'admin') {
       next();
     }
     else {
