d27a19675cc7539efd8dfd256e7e2ac93b2722cb
Mcvean Soans <mc.soans02@gmail.com>
2020-09-26 08:24:43 +0530

    User Authentication with Passport and JWT

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,7 +19,7 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => { console.log("Connected correctly to server"); }, (err) => { console.log(err); });
@@ -33,40 +34,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Using cookie-parser and signed cookies
-// app.use(cookieParser('12345-67890-09876-54321'));
-
-// Using Sessions instead of cookie-parser
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// Authorization Starts
-function auth (req, res, next) {
-    console.log(req.user);
-
-    if (!req.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      next(err);
-    }
-    else {
-      next();
-    }
-}
-
-app.use(auth);
-// Authorization ends
 
 app.use(express.static(path.join(__dirname, 'public')));
 
